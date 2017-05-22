3c97815741e1d2a5962d58049a4e74c91ce14345
sunyanguo <sunyanguo@lvmama.com>
2017-05-22 14:26:51 +0800

    Initial Commit

---
+++ ProjectOfUITest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectOfUITest
+//
+//  Created by yanguo sun on 22/05/2017.
+//  Copyright © 2017 Lvmama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectOfUITest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectOfUITest
+//
+//  Created by yanguo sun on 22/05/2017.
+//  Copyright © 2017 Lvmama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
